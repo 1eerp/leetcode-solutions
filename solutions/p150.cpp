f5583bf74c04b7de44ed775424b0abd221066a8c
Preetpal Singh <w3irdg@gmail.com>
2024-02-05 17:41:06 -0500

    changed indent from spaces to tab

--- solutions/p150.cpp
+++ solutions/p150.cpp
@@ -1,45 +1,45 @@
 #include "common.h"
 
 int evalRPN(vector<string>& tokens) {
-    int i = 0, opI = 0, n = tokens.size();
-    std::vector<int> nums(n, 0);
+	int i = 0, opI = 0, n = tokens.size();
+	std::vector<int> nums(n, 0);
 
-    for(;i<n; i++)
-    {
-        const string& temp = tokens[i];
-        if(isOperator(temp))
-        {
-            opI--;
-            nums[opI-1] = doOperation(temp[0], nums[opI-1], nums[opI]);
-        }
-        else
-        {
-            nums[opI] = std::stoi(temp);
-            opI++;
-        }
-    }
-    std::cout << endl;
-    return nums[0];
+	for(;i<n; i++)
+	{
+		const string& temp = tokens[i];
+		if(isOperator(temp))
+		{
+			opI--;
+			nums[opI-1] = doOperation(temp[0], nums[opI-1], nums[opI]);
+		}
+		else
+		{
+			nums[opI] = std::stoi(temp);
+			opI++;
+		}
+	}
+	std::cout << endl;
+	return nums[0];
 }
 
 bool isOperator(const std::string& s)
 {
-    return s[0] < 0x30 && s.size() == 1;
+	return s[0] < 0x30 && s.size() == 1;
 }
 
 int doOperation(const char c, int a, int b)
 {
-    switch(c)
-    {
-        case '+':
-            return a + b;
-        case '-':
-            return a - b;
-        case '*':
-            return a * b; 
-        case '/':
-            return a / b; 
-        default:
-            return 0;
-    }
+	switch(c)
+	{
+		case '+':
+			return a + b;
+		case '-':
+			return a - b;
+		case '*':
+			return a * b; 
+		case '/':
+			return a / b; 
+		default:
+			return 0;
+	}
 }
